9b89a3fe9a28279bcd1f951f18b6e649f7afb2a8
Andrew Reid <andrew.o.reid@gmail.com>
2024-12-27 16:45:22 +0000

    Create add task form and create function

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+    // sidenav initialisation
     $('.sidenav').sidenav();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    // datepicker initialisation
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialisation
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
+  });
